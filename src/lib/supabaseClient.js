934b96ea1f6551a928d237b12904785b310608a2
Hans Wilhelm Radam <radamhans15@gmail.com>
2026-04-04 18:33:00 +0800

    Test new feature

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tawqvyjnohhhquregbqm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd3F2eWpub2hoaHF1cmVnYnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMjgzMzIsImV4cCI6MjA4NzgwNDMzMn0.pPtHeE_ZcuTEs86k_LvzetkFl1FpCCwlRdCJipfWItI';
+//PROD 
+// const supabaseUrl = 'https://tawqvyjnohhhquregbqm.supabase.co';
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd3F2eWpub2hoaHF1cmVnYnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMjgzMzIsImV4cCI6MjA4NzgwNDMzMn0.pPtHeE_ZcuTEs86k_LvzetkFl1FpCCwlRdCJipfWItI';const supabaseUrl = 'https://tawqvyjnohhhquregbqm.supabase.co';
+
+const supabaseUrl = 'https://bubmtntqjofnokfwojri.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1Ym10bnRxam9mbm9rZndvanJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyODE0NzgsImV4cCI6MjA5MDg1NzQ3OH0.iQwujtT9RnpLpmFsCEZfWw4VVnrAww7JwdflL3YfksU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
